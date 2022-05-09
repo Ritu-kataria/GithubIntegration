80ac984919f9b69951804ca0501afece5e6bd158
Apple <apple@Apples-MacBook-Pro.local>
2022-05-09 17:57:21 +0530

    created login api

--- GithubIntegrationApp/ManageIssues/urls.py
+++ GithubIntegrationApp/ManageIssues/urls.py
@@ -1,8 +1,14 @@
 from django import views
-from django.urls import path
+from django.db import router
+from django.urls import path, include
 from ManageIssues import views
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', include(router.urls)),
     path('Issues', views.getIssues, name='Issues'),
 ]
--- GithubIntegrationApp/ManageIssues/views.py
+++ GithubIntegrationApp/ManageIssues/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import viewsets, status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 # Create your views here.
-def login(request):
-    return HttpResponse('This is login page')
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 def getIssues(request):
     return HttpResponse('This is getIssues page')
